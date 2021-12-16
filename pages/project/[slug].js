d1c38ac3f64ec00510bcaa716566d23abdc77835
Francois Huyghe <francois@13milliseconds.com>
2021-12-16 13:32:44 -0500

    modify return

--- pages/project/[slug].js
+++ pages/project/[slug].js
@@ -97,7 +97,7 @@ const Project = ({ params }) => {
 };
 
 export async function getStaticPaths() {
-  return;
+  return { paths: [], fallback: boolean };
 }
 
 export async function getStaticProps({ params }) {
