6ccad48de635de0d013144990e7ce2dbc8ca0e3a
Francois Huyghe <francois@13milliseconds.com>
2021-12-16 13:17:45 -0500

    add img alt failsafe

--- components/image.js
+++ components/image.js
@@ -7,7 +7,7 @@ const ImageEl = ({ image, style }) => {
   return (
     <Image
       src={imageUrl}
-      alt={image.alternativeText || image.name}
+      alt={image.alternativeText || image.name || ''}
       style={style}
     />
   );
