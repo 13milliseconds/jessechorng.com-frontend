f5bd9dd88a02f84be4715c8172e4f633c739bf55
Francois Huyghe <francois@13milliseconds.com>
2022-01-28 15:27:40 -0500

    add index as key

--- components/media-gallery.js
+++ components/media-gallery.js
@@ -5,7 +5,7 @@ const MediaGallery = ({ component }) => {
     <section className="media-gallery">
         <div className="uk-grid">
             <div className={`uk-child-width-1-${component.columns}@m uk-grid-match`} data-uk-grid>
-                {component.photo.map((photoBlock) => <MediaPhoto photoBlock={photoBlock} />)}
+                    {component.photo.map((photoBlock, i) => <MediaPhoto photoBlock={photoBlock} key={i}/>)}
             </div>
         </div>
     </section>
